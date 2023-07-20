89763b0d8508b952462180d32c4906be529baeb1
Delante Randolph <delantetr@Delantes-iMac.local>
2023-07-20 17:18:53 -0400

    fixed search bar and added seeds

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -6,25 +6,47 @@ const { Albums, Artists } = require('../../models');
 
 // Search route
 router.get('/', async (req, res) => {
-    const { query } = req.query;
-    console.log('Search query:', query);
-    
-    try {
-    // Search for albums with matching artist name or album title
-    const results = await Albums.findAll({
-        include: [
-            {  model: Artists },
-          ],
-          where: {
-            [Op.or]: [
-              { title: { [Op.like]: `%${query}%` } },
-              { '$artist.name$': { [Op.like]: `%${query}%` } },
-            ],
-          },
-        });
+  const { query } = req.query;
+  console.log('Search query:', query);
+  
 
-        console.log('Search result:', results);
-        res.json(results);
+  try {
+
+    let results;
+    let searchType;
+
+    results = await Artists.findAll({
+      where: {
+        name: { [Op.like]: `%${query}%` },
+      },
+    });
+
+    if (results.length > 0) {
+      // const finalresults = results.map((result) => result.get({ plain: true }));
+      searchType = 'artist';
+    } else {
+      results = await Albums.findAll({
+        where: {
+          [Op.or]: [
+            { title: { [Op.like]: `%${query}%` } },
+          ],
+        },
+      });
+      searchType = 'album';
+    }
+
+    console.log('Search result:', results);
+
+    const searchData = results.map((result) => ({
+      type: searchType,
+      id: result.id,
+    }));
+
+    console.log(searchData);
+    // console.log('Search type:', type);
+    
+    res.json(searchData);
+    // res.render('artist-album', results);
   } catch (error) {
     console.error('Error during search:', error);
     res.status(500).json({ error: 'Failed to perform search.' });
--- seeds/artistData.js
+++ seeds/artistData.js
@@ -10,6 +10,45 @@ const artistdata = [
     {
       name: 'Chris Stapleton',    
     },
+    {
+      name: 'Beyonce',    
+    },
+    {
+      name: 'Nicki Minaj',    
+    },
+    {
+      name: "Lil' Wayne",    
+    },
+    {
+      name: "Ice Spice",    
+    },
+    {
+      name: "Luke Bryan",    
+    },
+    {
+      name: "Red Hot Chili Peppers",    
+    },
+    {
+      name: "Good Charlotte",    
+    },
+    {
+      name: "System of A Down",    
+    },
+    {
+      name: "Metallica",    
+    },
+    {
+      name: "Charlie Parker",    
+    },
+    {
+      name: "Bad Bunny",    
+    },
+    {
+      name: "Deadmau5",    
+    },
+    {
+      name: "Deadmau5",    
+    },
 ];
 
 const seedArtists = () => Artists.bulkCreate(artistdata);
